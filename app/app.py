e9f1ec3c93e94c43435f0de4d67d94163fd8d18e
yuvanreddy <raajvardhanreddy@gmail.com>
2026-01-16 19:01:25 +0530

    ok

--- app/app.py
+++ app/app.py
@@ -2,21 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import socket, os
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", "mysecretkey")   # required for sessions
+app.secret_key = os.getenv("SECRET_KEY", "secret123")
 
-# Dummy users (later we can store in DB)
+# Dummy users (you can later connect DB)
 USERS = {
     "admin": "admin123",
     "yuvan": "yuvan123"
 }
 
-@app.route('/')
+@app.route("/")
 def home():
     if "user" in session:
         return redirect(url_for("dashboard"))
     return redirect(url_for("login"))
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form.get("username")
@@ -26,11 +26,11 @@ def login():
             session["user"] = username
             return redirect(url_for("dashboard"))
 
-        return render_template("login.html", error="Invalid username/password")
+        return render_template("login.html", error="Invalid Username/Password")
 
     return render_template("login.html")
 
-@app.route('/dashboard')
+@app.route("/dashboard")
 def dashboard():
     if "user" not in session:
         return redirect(url_for("login"))
@@ -42,14 +42,14 @@ def dashboard():
         version=os.getenv("APP_VERSION", "v1")
     )
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     session.pop("user", None)
     return redirect(url_for("login"))
 
-@app.route('/health')
+@app.route("/health")
 def health():
     return {"status": "ok"}, 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
